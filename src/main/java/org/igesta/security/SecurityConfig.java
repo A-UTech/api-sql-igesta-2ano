f1a475bd0294e33dd276fc45bf3d6071008b43c3
norgetz <samukagerminare@gmail.com>
2025-10-26 20:01:09 -0300

    Arrumando a forma como o security pega a .env para mandar para o Render

--- src/main/java/org/igesta/security/SecurityConfig.java
+++ src/main/java/org/igesta/security/SecurityConfig.java
@@ -1,12 +1,10 @@
 package org.igesta.security;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,42 +15,45 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
-    CustomAccessDeniedHandler customAcessDeniedHandler;
 
-    public SecurityConfig(CustomAccessDeniedHandler customAcessDeniedHandler) {
-        this.customAcessDeniedHandler = customAcessDeniedHandler;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    public SecurityConfig(CustomAccessDeniedHandler customAccessDeniedHandler) {
+        this.customAccessDeniedHandler = customAccessDeniedHandler;
     }
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager (PasswordEncoder enconder){
-        Dotenv dotenv = Dotenv.load();
-        String adminUsername = dotenv.get("API_ADMIN_USERNAME");
-        String adminPassword = dotenv.get("API_ADMIN_PASSWORD");
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder encoder) {
+        String adminUsername = System.getenv("API_ADMIN_USERNAME");
+        String adminPassword = System.getenv("API_ADMIN_PASSWORD");
+
         UserDetails admin = User.withUsername(adminUsername)
-                .password(enconder.encode(adminPassword))
+                .password(encoder.encode(adminPassword))
                 .roles("ADMIN")
                 .build();
+
         return new InMemoryUserDetailsManager(admin);
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/igesta/public/**").permitAll()
                         .requestMatchers("/igesta/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                .httpBasic(Customizer.withDefaults())
                 .exceptionHandling(exception -> exception
-                        .accessDeniedHandler(customAcessDeniedHandler))
-                .httpBasic(Customizer.withDefaults());
+                        .accessDeniedHandler(customAccessDeniedHandler)
+                );
+
         return http.build();
     }
-
-
 }
