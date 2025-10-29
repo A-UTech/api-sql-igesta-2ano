31bfd3cccfd4f6a9d1c82248cb48456c84c037dc
norgetz <samukagerminare@gmail.com>
2025-10-29 10:16:08 -0300

    feat/selecionar-por-unidade-e-hora: Criando um método de seleção por idUnidade e horário de início e de fim

--- src/main/java/org/igesta/repository/TurnoRepository.java
+++ src/main/java/org/igesta/repository/TurnoRepository.java
@@ -3,5 +3,11 @@ package org.igesta.repository;
 import org.igesta.model.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+
 public interface TurnoRepository extends JpaRepository<Turno, Long> {
+
+    List<Turno> findByIdUnidadeAndInicioAndFim(Long idUnidade, LocalTime inicio, LocalTime fim);
 }
